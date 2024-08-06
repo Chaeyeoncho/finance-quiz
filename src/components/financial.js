313ecccd14e0236b12c68fb045529b75cf111177
Chaeyeoncho <gcy9293@naver.com>
2024-08-06 22:29:58 +0900

    feat : 선택 페이지 만들기

---
+++ src/components/financial.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Quiz.css";
+
+const keywords = [
+  "예산 관리",
+  "저축",
+  "투자",
+  "신용 점수",
+  "부채",
+  "보험",
+  "금리",
+  "주식",
+  "채권",
+  "펀드",
+  "부동산",
+  "세금",
+  "연금",
+  "리스크 관리",
+  "환율",
+  "자산 배분",
+  "재무제표",
+  "현금 흐름",
+  "기업가치평가",
+  "파생상품",
+  "비트코인",
+];
+
+const Quiz = () => {
+  return (
+    <div className="quiz-container">
+      <h2>학습할 키워드를 </h2>
+      <h2>선택해주세요</h2>
+      <h3>원하는 키워드를 입력해주세요</h3>
+      <div className="keywords-container">
+        {keywords.map((keyword, index) => (
+          <div key={index} className="keyword-button">
+            {keyword}
+          </div>
+        ))}
+      </div>
+      <button className="generate-quiz-button">퀴즈 생성하기</button>
+    </div>
+  );
+};
+
+export default Quiz;
